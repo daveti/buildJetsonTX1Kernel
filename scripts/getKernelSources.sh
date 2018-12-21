04adaff20004fe3fa69034b0083c6569845da574
Dave Tian <dave.jing.tian@gmail.com>
2018-12-21 19:59:05 +0000

    used sdcard for kbuild

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -2,7 +2,8 @@
 apt-add-repository universe
 apt-get update
 apt-get install qt5-default pkg-config -y
-cd /usr/src
+#cd /usr/src
+cd ~daveti/home2/l4t
 wget -N http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/GA/BSP/tx1_sources.tbz2
 echo "Expanding sources, please wait"
 tar -xvf tx1_sources.tbz2 public_release/kernel_src.tbz2
