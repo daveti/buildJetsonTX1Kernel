0263031727962476ac844081964828e40637af82
Dave Tian <dave.jing.tian@gmail.com>
2018-12-29 23:54:33 +0000

    beautify the kbuild

--- kbuild.sh
+++ kbuild.sh
@@ -6,17 +6,20 @@
 # Clear the build dir
 echo "CLEARING..."
 sudo ./removeAllKernelSources.sh
+echo "================================"
 
 # Get the kernel source
 echo "GETTING SRC..."
 sudo ./getKernelSources.sh
+echo "================================"
 
 # Build the kernel
 echo "BUILDING..."
 sudo ./scripts/makeKernel.sh
+echo "================================"
 
 # Install the image
 echo "IMAGING..."
 sudo ./copyImage.sh
-
+echo "================================"
 echo "DONE"
