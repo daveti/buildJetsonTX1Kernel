bf322cca92b4f7ddfdfc549d5782ce7991bfcb6d
Dave Tian <dave.jing.tian@gmail.com>
2018-12-31 01:54:04 +0000

    dir update on copyImage

--- copyImage.sh
+++ copyImage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Check to make sure the Image file has been generated
 # The "could not stat" error seems to confuse people
-if [ -f /usr/src/kernel/kernel-4.4/arch/arm64/boot/Image ] ; then
+#if [ -f /usr/src/kernel/kernel-4.4/arch/arm64/boot/Image ] ; then
+if [ -f ~daveti/home2/l4t/kernel/kernel-4.4/arch/arm64/boot/Image ] ; then
   echo "Copying Image to /boot diretory"
   sudo ./scripts/copyImage.sh
   echo "Image copied"
